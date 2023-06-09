19daa7a7c9d43ad91dd5c00cc1b1b5e9ba071c21
Polina Zakomlistova <polina_zakomlistova@mail.ru>
2023-06-09 18:03:32 +0700

    initial commit

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,16 @@
+type Mods = Record<string, boolean | string>;
+
+export function classNames(
+    cls: string,
+    mods: Mods,
+    additional: string[]
+): string {
+    return [
+        cls,
+        ...additional,
+        ...Object.entries(mods) //разбираем на картеж [[key,value],[key,value]]
+            .filter(([className, value]) => Boolean(value)) //отфильтровываем только true
+            .map(([className]) => className), //массив классов
+    ].join(' ');
+}
+// classNames( 'remove-btn', {hovered: false, red: true, ['pdg']})
