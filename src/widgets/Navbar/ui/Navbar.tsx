87ccf3f14556c4a80b8e2ec35f07f2024f600591
Polina Zakomlistova <polina_zakomlistova@mail.ru>
2023-11-02 16:07:44 +0200

    add modal, portal, fixed themeDecorator, change navBar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
+import React, { useCallback, useState } from 'react';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -9,10 +11,20 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
+    const [isOpenAuthModal, setIsOpenAuthModal] = useState(false);
+    const onToggleModal = useCallback(() => setIsOpenAuthModal((prev) => !prev), []);
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            {}
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            {/* eslint-disable-next-line i18next/no-literal-string */}
+            <Modal isOpen={isOpenAuthModal} onClose={onToggleModal}>Lorem</Modal>
         </div>
     );
 };
